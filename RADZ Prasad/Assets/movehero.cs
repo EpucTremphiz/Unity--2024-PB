4648b7b5103709c3824707b769e9b665aee0ebd5
STUDENT Prasad Balkerishna <Prasad.Balkerishna@students.ittralee.ie>
2024-10-02 12:52:00 +0100

    Signed-off-by: STUDENT Prasad Balkerishna <Prasad.Balkerishna@students.ittralee.ie>

--- RADZ Prasad/Assets/movehero.cs
+++ RADZ Prasad/Assets/movehero.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class movehero : MonoBehaviour
 {
+    float runningSpeed = 5f;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
+        if (animator == null)
+            print("Could not find Animator Component");
+        else
+            print("Animator Component found");
+   
+    
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("is running", false);
         if (Input.GetKey(KeyCode.W))
-            transform.position += Vector3.forward * Time.deltaTime;
+        {
+            transform.position += runningSpeed * transform.forward * Time.deltaTime;
+            animator.SetBool("is running", true);
+        }
 
         if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.down, 30 * Time.deltaTime); 
+            transform.Rotate(Vector3.down, 50 * Time.deltaTime); 
 
         if (Input.GetKey(KeyCode.S))
             transform.position += Vector3.back * Time.deltaTime;
 
         if(Input.GetKey(KeyCode.D))
-            transform.Rotate(Vector3.up, 30 * Time.deltaTime);
+            transform.Rotate(Vector3.up, 50 * Time.deltaTime);
     }
 
 
